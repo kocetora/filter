5513a64bb7f5e36d19fd849912797521533e93d6
kocetora <kocetora@gmail.com>
2021-07-08 22:41:16 +0300

    fix: polyfills and main before working withot zone.js

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,9 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    ngZone: 'noop'
+  })
   .catch(err => console.error(err));
+
